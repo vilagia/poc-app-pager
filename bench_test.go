b2cb93a3dfa77450ee4b1f5aea31ae69296aa3b8
vilagia <99010581+vilagia@users.noreply.github.com>
2024-12-22 00:38:39 +0900

    test: more fine grained

--- bench_test.go
+++ bench_test.go
@@ -69,6 +69,63 @@ func Benchmark_QueryWithFilterPass10Percent(b *testing.B) {
 	b.Log("average", averageRetryCount)
 }
 
+func Benchmark_QueryWithFilterPass5Percent(b *testing.B) {
+	b.ResetTimer()
+	retryCounts := []uint{}
+	for range b.N {
+		retryCounts = append(retryCounts, findWithRefetch(5))
+	}
+
+	averageRetryCount := lo.Mean(retryCounts)
+	b.Log("average", averageRetryCount)
+}
+
+func Benchmark_QueryWithFilterPass4Percent(b *testing.B) {
+	b.ResetTimer()
+	retryCounts := []uint{}
+	for range b.N {
+		retryCounts = append(retryCounts, findWithRefetch(4))
+	}
+
+	averageRetryCount := lo.Mean(retryCounts)
+	b.Log("average", averageRetryCount)
+}
+
+
+func Benchmark_QueryWithFilterPass3Percent(b *testing.B) {
+	b.ResetTimer()
+	retryCounts := []uint{}
+	for range b.N {
+		retryCounts = append(retryCounts, findWithRefetch(3))
+	}
+
+	averageRetryCount := lo.Mean(retryCounts)
+	b.Log("average", averageRetryCount)
+}
+
+
+func Benchmark_QueryWithFilterPass2Percent(b *testing.B) {
+	b.ResetTimer()
+	retryCounts := []uint{}
+	for range b.N {
+		retryCounts = append(retryCounts, findWithRefetch(2))
+	}
+
+	averageRetryCount := lo.Mean(retryCounts)
+	b.Log("average", averageRetryCount)
+}
+
+func Benchmark_QueryWithFilterPass1Percent(b *testing.B) {
+	b.ResetTimer()
+	retryCounts := []uint{}
+	for range b.N {
+		retryCounts = append(retryCounts, findWithRefetch(1))
+	}
+
+	averageRetryCount := lo.Mean(retryCounts)
+	b.Log("average", averageRetryCount)
+}
+
 func findWithRefetch(threshould uint) uint {
 	result := []repository.Foo{}
 
